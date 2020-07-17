63737f239c2f4c96f942c1ec1a0d0f739eb8af2a
TejaswiniSekharamantri <stejaswini103@gmail.com>
2020-07-17 21:57:27 +0530

    Updates

---
+++ precache-manifest.6889592d1557f51d8752f5746154176d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2495b6d0b548c50afe87907a5271b303",
+    "url": "/gif/index.html"
+  },
+  {
+    "revision": "44d557d6d5beeee3e353",
+    "url": "/gif/static/css/2.f46c9330.chunk.css"
+  },
+  {
+    "revision": "47dd0eb67d6aca0858fc",
+    "url": "/gif/static/css/main.4c8fd359.chunk.css"
+  },
+  {
+    "revision": "44d557d6d5beeee3e353",
+    "url": "/gif/static/js/2.3219d2fe.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gif/static/js/2.3219d2fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47dd0eb67d6aca0858fc",
+    "url": "/gif/static/js/main.7ed67d3f.chunk.js"
+  },
+  {
+    "revision": "738bf54e1cbddac9960e",
+    "url": "/gif/static/js/runtime-main.28baee91.js"
+  }
+]);
